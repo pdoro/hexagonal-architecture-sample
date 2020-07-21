0753455e5409b6492c2782c84617e37aef9e4954
Pablo Domingo Rojo <pdomingo@WPU8L0070968.ad.ing.net>
2020-07-21 13:58:41 +0200

    Removed unnecessary directory

--- hex-arch-sample/src/main/java/com/cel/hexarchsample/domain/port/primary/ComputeSubtotal.java
+++
@@ -1,9 +0,0 @@
-package com.cel.hexarchsample.domain.port.primary;
-
-import com.cel.hexarchsample.domain.model.vobjects.Money;
-import com.cel.hexarchsample.domain.model.vobjects.ids.CustomerId;
-
-public interface ComputeSubtotal {
-
-    Money computeSubtotalFor(CustomerId customerId);
-}
--- hex-arch-sample/src/main/java/com/cel/hexarchsample/infrastructure/exception/CustomerNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.cel.hexarchsample.infrastructure.exception;
-
-public class CustomerNotFoundException extends BaseInfrastructureException {
-
-}
